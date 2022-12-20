63339d6a505f91eb1ec5259a48a909a5f97ec959
Hariyanto Wibowo <hariwib@darmajaya.ac.id>
2022-12-20 07:48:11 +0700

    menambahkan file config.php dan index.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    $databasehost = "localhost";
+    $databaseuser = "root";
+    $databasepwd  = "";
+    $databasename ="dbpl";
+
+    $mysql = mysqli_connect($databasehost,$databaseuser,$databasepwd,$databasename);
+
+?>
